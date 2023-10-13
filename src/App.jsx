1015c0767c4dcb592dc792e125cc84fb7e0d055e
Ayush Anand <ayushanand141@gmail.com>
2023-10-13 11:23:30 +0530

    13th Oct

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import Sidebar from "./components/sidebar/Sidebar";
 
 const App = () => {
   return <div>
-    <section> <Navbar/> </section>
-    <section>Parallax</section>
-    <section>Services</section>
+    <section id='Homepage'> <Navbar/> </section>
+    <section id='Services'>Parallax</section>
+    <section id='Portfolio'>Services</section>
     <section>Parallax</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section>Contact</section>
+    <section id='Contact'>Contact</section>
 
     {/* <Test/>
     <Test/> */}
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -4,8 +4,11 @@ const Links = () => {
 
 const items=["Homepage","Services","Portfolio","Contact","About"]
   return (
-    <div className="links"> {items.map(item=>(
-        <a href={`#${item}`} key={item}></a>
+    <div className="links"> 
+    {items.map(item=>(
+        <a href={`#${item}`} key={item}>
+            {item}
+        </a>
     ))} </div>
   )
 }

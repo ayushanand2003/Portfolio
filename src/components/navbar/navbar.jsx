54d807783fabb4bba12eca8d5e7d7ca302b40c1d
Ayush Anand <ayushanand141@gmail.com>
2023-10-14 16:40:51 +0530

    14th Oct

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,26 +1,34 @@
 import Sidebar from "../sidebar/Sidebar";
-import "./navbar.scss"
-import {motion} from "framer-motion";
+import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-    return(
-        <div className="navbar">
-            {/* sidebar */}
-            <Sidebar/>
-            
-            <div className="wrapper">
-                <motion.span initial={{opacity:0,scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:0.5}}>Ayush Anand</motion.span>
-                
-                
-                <div className="social">
-                    <a href="#"><img src="/github.png" alt="Github" /></a>
-                    <a href="#"><img src="/linkedin.png" alt="Linkedin" /></a>
-                    <a href="#"><img src="/instagram.png" alt="Insta" /></a>            
-                </div>
-            </div>
-            
-    
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          AYUSH ANAND
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/linkedin.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/github.png" alt="" />
+          </a>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,37 +1,38 @@
-import "./sidebar.scss";
-import Links from "./links/Links";
-import ToggleButton from "./toggleButton/ToggleButton";
-import { motion } from "framer-motion";
 import { useState } from "react";
+import { motion } from "framer-motion";
+import Links from "./links/Links";
+import "./sidebar.scss";
+import ToggleButton from "./toggleButton/ToggleButton";
 
-const variants={
-  open:{
+const variants = {
+  open: {
     clipPath: "circle(1200px at 50px 50px)",
-    type: "spring",
-    stiffness: 20,
-  },
-  closed:{
-    clipPath:"circle(30px at 50px 50px)",
-    transition:{
-      delay:0.5,
-      type:"spring",
-      stiffness:400,
-      damping:40,
+    transition: {
+      type: "spring",
+      stiffness: 20,
     },
-  }
-}
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
 const Sidebar = () => {
-
   const [open, setOpen] = useState(false);
 
   return (
-    <motion.div className="sidebar" animate={open ? "open": "closed"}>
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
       <motion.div className="bg" variants={variants}>
-          <Links/>
+        <Links />
       </motion.div>
       <ToggleButton setOpen={setOpen} />
     </motion.div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;

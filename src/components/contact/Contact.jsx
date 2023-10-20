8b336bb288561cf1d484d9cf5ceeae444ddcc59d
Ayush Anand <ayushanand141@gmail.com>
2023-10-20 21:51:45 +0530

    Updated Changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -31,10 +31,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_94y20xo",
-        "template_v10u2oh",
+        "service_payy8lg",
+        "template_mxsmous",
         formRef.current,
-        "pX_2hasGmGcuvjXIW"
+        "xf3m9yMtJWPEXPk0w"
       )
       .then(
         (result) => {
@@ -58,15 +58,15 @@ const Contact = () => {
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
-          <span>hello@react.dev</span>
+          <span>ayan.10xdev@gmail.com</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Hello street New York</span>
+          <span>Bangalore,Karnataka(INDIA)</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
-          <span>+1 234 5678</span>
+          <span>Available upon Mail/DM</span>
         </motion.div>
       </motion.div>
       <div className="formContainer">

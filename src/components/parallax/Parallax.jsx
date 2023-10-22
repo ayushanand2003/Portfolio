fbdc435419762dc3828f5d44bfbc8db8ac620850
Ayush Anand <ayushanand141@gmail.com>
2023-10-22 17:17:18 +0530

    Updated Parallax

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -4,6 +4,29 @@ import { motion, useScroll, useTransform } from "framer-motion";
 
 const Parallax = ({ type }) => {
   const ref = useRef();
+  const textVariants = {
+    initial: {
+      x: -500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.1,
+      },
+    },
+    scrollButton: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+      },
+    },
+  };
+  
 
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -21,12 +44,22 @@ const Parallax = ({ type }) => {
         background:
           type === "services"
             ? "linear-gradient(180deg, #111132, #0c0c1d)"
-            : "linear-gradient(180deg, #111132, #505064)",
+            : "linear-gradient(180deg, #110132, #501064)",
       }}
     >
+      <motion.div className="img">
+
+      <motion.img
+      variants={textVariants}
+      animate="scrollButton"
+      src="https://raw.githubusercontent.com/ayushanand2003/Portfolio/main/public/scroll.png"
+      alt=""
+    />
+    </motion.div>  
       <motion.h1 style={{ y: yText }}>
         {type === "services" ? "LATEST WORK" : "ALL PROJECTS"}
       </motion.h1>
+      
       <motion.div className="mountains"></motion.div>
       <motion.div
         className="cartoons"
@@ -37,7 +70,11 @@ const Parallax = ({ type }) => {
           })`,
         }}
       ></motion.div>
+      
       <motion.div style={{ x: yBg }} className="stars"></motion.div>
+      
+      
+      
     </div>
   );
 };
